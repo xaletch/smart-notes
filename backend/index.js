b63f99e55419b13b3e3556e74103f08047dda743
xaletch <kiril.kolesnikov0609@mail.ru>
2023-10-22 23:45:52 +0300

    attempt to load data received on the frontend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,49 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const AppUrl = 'https://www.twitch.tv/'
+const express = require('express');
+const app = express();
+const multer = require('multer');
+const path = require('path');
+
+const PORT = 8000;
+
+app.get('/', (req, res) => {
+    res.send('Home');
+});
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  }),
+});
+let notesArray = [];
+
+app.post('/', upload.single('file'), (req, res) => {
+  const notes = JSON.parse(req.body.notes);
+
+  notesArray.push(notes);
+
+  res.send('NOTES SAVED');
+});
+
+app.get('/notes', (req, res) => {
+  res.send(notesArray);
+});
+
+app.listen(PORT, (err) => {
+  console.log('so far everything is working like clockwork');
+
+  if (err) {
+      console.log(err);
+  };
+});
+
+const AppUrl = 'https://xaletch.github.io/frontend/';
 
 const token = '6361296205:AAEhdzgUuuEYlfLsWyLkKhN5ct3wO4yth2w';
 
@@ -13,11 +56,11 @@ bot.on('message', async (msg) => {
 
   if (text === '/start') {
     await bot.sendMessage(chatId, `${username}, привет! Здесь ты сможешь создавать и управлять своими планами на ближайший день, неделю, месяц и год.`, {
-        reply_markup: {
-            inline_keyboard: [
-                [{text: 'Создать распорядок дня', web_app: {url: AppUrl}}]
-            ]
-        }
+      reply_markup: {
+        inline_keyboard: [
+          [{text: 'Создать распорядок дня', web_app: {url: AppUrl}}]
+        ]
+      }
     })
   };
 });
---
+++ backend/notes.js
@@ -0,0 +1,1 @@
+export const test = (req, res) => {}
